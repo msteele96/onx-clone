809fd29aa17e46fee383aa7e32249966442a3be6
msteele96 <marcsteele2014@gmail.com>
2021-07-04 21:04:34 -0400

    Cleaned up extra from redux template, added reducer

--- onx-clone-frontend/src/index.js
+++ onx-clone-frontend/src/index.js
@@ -2,20 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { store } from './app/store';
 import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk'
+import usersReducer from './reducers/usersReducer';
+
+
+const store = createStore(usersReducer, applyMiddleware(thunk))
+
+// const store = createStore(usersReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+// for use with devtools but no thunk
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
