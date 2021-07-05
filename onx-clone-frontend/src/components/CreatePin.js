a340d672d25189667a27a53945f59541f81b457f
msteele96 <marcsteele2014@gmail.com>
2021-07-05 18:28:00 -0400

    Added lat/long and default option for type in CreatePin and tested dummy submit

--- onx-clone-frontend/src/components/CreatePin.js
+++ onx-clone-frontend/src/components/CreatePin.js
@@ -37,7 +37,8 @@ export default class CreatePin extends Component {
         return (
             <form onSubmit={this.handleSubmit}>
                 <label>Type: </label>
-                <select name="style">
+                <select name="style" onChange={this.handleChange}>
+                    <option value=""></option>
                     <option value="tracks">Tracks</option>
                     <option value="stand">Stand</option>
                     <option value="Blind">Blind</option>
@@ -46,9 +47,9 @@ export default class CreatePin extends Component {
                 <label>Name: </label>
                 <input type="text" value={this.state.name} name="name" onChange={this.handleChange}/>
                 <label>Latitude: </label>
-
+                <input type="number" min="-180" max="180" step=".000001" name="latitude" value={this.state.latitude} onChange={this.handleChange}/>
                 <label>Longitude: </label>
-
+                <input type="number" min={-180} max={180} step=".000001" name="longitude" value={this.state.longitude} onChange={this.handleChange}/>
                 <label>Notes: </label>
                 <textarea value={this.state.notes} name="notes" onChange={this.handleChange}/>
                 <input type="submit"/>
