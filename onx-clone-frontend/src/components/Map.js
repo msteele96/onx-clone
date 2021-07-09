0278b5dfdb94fc2278b8b9367217064575bc7be3
msteele96 <marcsteele2014@gmail.com>
2021-07-09 15:50:39 -0400

    Changed map type to satellite

--- onx-clone-frontend/src/components/Map.js
+++ onx-clone-frontend/src/components/Map.js
@@ -1,20 +1,3 @@
-// import { Component } from "react";
-
-// export default class Map extends Component {
-//     constructor(props) {
-//         super()
-//         // this.state = {}
-//     }
-
-//     render() {
-//         return (
-//             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d207559.4085183397!2d-83.8837966788842!3d35.624872678687574!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8859521e1372343d%3A0xa04eac8607697ce3!2sGreat%20Smoky%20Mountains!5e0!3m2!1sen!2sus!4v1625526467509!5m2!1sen!2sus" width="800" height="600" title="map" allowfullscreen="" loading="lazy"></iframe>
-//         )
-//     }
-// }
-// // mapStateToProps to get pins in to display on map somehow
-
-
 import React from 'react';
 import H from "@here/maps-api-for-javascript";
 import onResize from 'simple-element-resize-detector';
@@ -54,7 +37,9 @@ export default class Map extends React.Component {
       const layers = platform.createDefaultLayers();
       const map = new H.Map(
         this.ref.current,
-        layers.vector.normal.map,
+        // layers.raster.normal.map,
+        layers.raster.satellite.map,
+        // layers.raster.terrain.map,
         {
           pixelRatio: window.devicePixelRatio,
           center: {lat: 0, lng: 0},
