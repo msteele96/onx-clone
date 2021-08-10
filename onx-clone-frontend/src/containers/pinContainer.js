794e2d352e423dff6527da1da61758954f385632
msteele96 <marcsteele2014@gmail.com>
2021-08-09 21:10:27 -0400

    Added button to sort pins alphabetically on name

--- onx-clone-frontend/src/containers/pinContainer.js
+++ onx-clone-frontend/src/containers/pinContainer.js
@@ -3,6 +3,12 @@ import CreatePin from "../components/CreatePin";
 import Pin from "../components/Pin";
 
 export default class PinContainer extends Component {
+    // constructor(props) {
+    //     super(props)
+    //     this.state = {
+    //         pins: this.props.pins
+    //     }
+    // }
     
     componentDidMount() {
         if (this.props.user.data.id !== 0) {
@@ -14,11 +20,30 @@ export default class PinContainer extends Component {
         return this.props.pins.map(pin => <Pin key={pin.id} pin={pin}/>)
     }
 
+    sortPins = () => {
+        let a = this.props.pins.sort((a, b) => {
+            let fa = a.name.toLowerCase(),
+                fb = b.name.toLowerCase();
+        
+            if (fa < fb) {
+                return -1;
+            }
+            if (fa > fb) {
+                return 1;
+            }
+            return 0;
+        });
+        this.setState({
+            pins: a
+        })
+    }
+
     render() {
         return (
             <div>
                 <CreatePin addPin={this.props.addPin} user={this.props.user}/>
                 <h2>Pins</h2>
+                <button onClick={this.sortPins}>Sort Pins A-Z</button>
                 <ul>
                     {this.renderPins()}
                 </ul>
